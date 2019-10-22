bc123ed4099645f8d272a233fdc21436acbf87c8
Peter Wu <23109224+PeterBWu@users.noreply.github.com>
2019-10-22 12:00:12 -0700

    modified ladder to accept props

--- client/src/Components/Ladder/index.js
+++ client/src/Components/Ladder/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-const Ladder = () => {
+
+// import Col from 'react-bootstrap/Col'
+// import Row from 'react-bootstrap/Row'
+
+import LadderStep from './../LadderStep'
+
+
+const Ladder = (props) => {
     return(
         <div>
-            <h1>Ladder Div</h1>
+            {
+                props.challengers.map((challenger,index) => <LadderStep key={index} active={props.round===(props.challengers.length-index) ? "true":"false"} fighter={challenger} />)
+            }
+            
         </div>
     )
 }
+
 
 export default Ladder
