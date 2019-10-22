c376cd553669ed26dde3a632b601a28628217d1e
Peter Wu <23109224+PeterBWu@users.noreply.github.com>
2019-10-22 03:54:55 -0700

    created basic layout for transition screen

---
+++ client/src/Components/Ladder/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+const Ladder = () => {
+    return(
+        <div>
+            <h1>Ladder Div</h1>
+        </div>
+    )
+}
+
+export default Ladder
---
+++ client/src/Components/Shop/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Shop = () => {
+    return(
+        <div>
+            <button>Shop</button>
+            <button>Next Battle</button>
+        </div>
+    )
+}
+
+export default Shop
