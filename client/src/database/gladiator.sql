a30afc431a396d969ebeeb8c9be12f22b9756059
Rob <Rob.Kutner@Gmail.com>
2019-10-22 13:02:33 -0700

    populated images for leaders, working on stats

--- client/src/database/gladiator.sql
+++ client/src/database/gladiator.sql
@@ -3,14 +3,6 @@ DROP DATABASE IF EXISTS gladiator_db;
 CREATE DATABASE gladiator_db;
 USE gladiator_db;
 
-CREATE TABLE character(
-character_id INT NOT NULL AUTO_INCREMENT,
-character_portrait VARCHAR(255) NOT NULL, 
-character_name VARCHAR(50) NOT NULL,
-character_hp_max INT DEFAULT 10 NOT NULL,
-PRIMARY KEY (character_id)
-);
-
 CREATE TABLE leaderboard(
 leader_id INT AUTO_INCREMENT NOT NULL,
 leader_name VARCHAR(50) NOT NULL,
@@ -18,3 +10,12 @@ leader_portrait VARCHAR(255) NOT NULL,
 leader_rank INT NOT NULL,
 PRIMARY KEY(leader_id)
 );
+
+CREATE TABLE items(
+    item_id INT AUTO_INCREMENT NOT NULL,
+    item_name VARCHAR(100) NOT NULL,
+    item_img VARCHAR(255)NOT NULL, 
+    item_atk INT NOT NULL,
+    item_def INT NOT NULL,
+    PRIMARY KEY (item_id)
+)
