a381670b142e944237d7f638fabbc3015b619c67
Andrew Jolivette <amjolivette@gmail.com>
2019-10-22 12:33:03 -0700

    created initial layout for start page

--- client/src/pages/Start/index.js
+++ client/src/pages/Start/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Start() {
-  return (
-    <h1>I am the Start Screen</h1>
-  )
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+
+import styles from "./index.module.css"
+import TitleCard from "./pictures/ph.png"
+
+class Start extends Component {
+
+  render() {
+    
+    return (
+      <Container className="vh-100 d-flex justify-content-center">
+        <Row className={styles.cardRow}>
+          <Col xs={12}> 
+          <Card className={styles.cardSize}>
+            <Card.Img src={TitleCard} fluid />
+            <Card.Body>
+              <Card.Subtitle className="mb-2 text-muted text-center">Vincit qui se vincit.</Card.Subtitle>
+              <Row className="mt-5">
+                <Col>
+                  <Row>Current Champion Component Goes Here</Row>
+                  <Row>
+                    <Button 
+                    >
+                        View Stats
+                    </Button>
+                  </Row>
+                </Col>
+                <Col className="text-center">
+                  <Button size="lg">Create Challenger</Button>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default Start;
