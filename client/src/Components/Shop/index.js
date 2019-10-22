e4db0809dd8910e6fd13be5fbe3485d89395a2a8
Peter Wu <23109224+PeterBWu@users.noreply.github.com>
2019-10-22 04:19:42 -0700

    buttons now between load screen and shop

--- client/src/Components/Shop/index.js
+++ client/src/Components/Shop/index.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+class Shop extends Component{
+    state = {
+        inShop: false
+    }
 
-const Shop = () => {
-    return(
-        <div>
-            <button>Shop</button>
-            <button>Next Battle</button>
-        </div>
-    )
+    loadButtons = () =>{
+        return(
+            <button>pot</button>
+        )
+    }
+
+    loadShop = () => {
+        return(
+            <div>
+                <hi>The shop</hi>
+                {this.loadButtons()}
+                {this.loadButtons()}
+                {this.loadButtons()}
+            </div>
+        )
+    }
+    loadWaitScreen = () => {
+        return(
+            
+            <div>
+                <button onClick={this.handleOnShopClick}>Shop</button>
+                <button>Next Battle</button>
+            </div>
+        )
+    }
+
+    loadScreen = () => {this.state.inShop ? this.loadShop(): this.loadWaitScreen()}
+
+    handleOnShopClick = () => {
+        this.setState({inShop:true})
+    }
+
+    render(){
+        return(
+            <div>
+               {this.state.inShop ? this.loadShop(): this.loadWaitScreen()}
+            </div>
+        )
+    }
 }
 
 export default Shop
