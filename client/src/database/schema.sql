86558403f69a1ec27d6dcab1846b3fffe06b44ba
Rob <Rob.Kutner@Gmail.com>
2019-10-21 19:55:21 -0700

    added protection against empty cells in database

--- client/src/database/schema.sql
+++ client/src/database/schema.sql
@@ -4,17 +4,17 @@ CREATE DATABASE gladiator_db;
 USE gladiator_db;
 
 CREATE TABLE fighter(
-fighter_id INT AUTO_INCREMENT,
-figher_portrait VARCHAR(255), 
-fighter_name VARCHAR(50),
-fighter_hp_max INT DEFAULT 10,
+fighter_id INT NOT NULL AUTO_INCREMENT,
+figher_portrait VARCHAR(255) NOT NULL, 
+fighter_name VARCHAR(50) NOT NULL,
+fighter_hp_max INT DEFAULT 10 NOT NULL,
 PRIMARY KEY (fighter_id)
 );
 
 CREATE TABLE leaderboard(
-leader_id INT AUTO_INCREMENT,
-leader_name VARCHAR(50),
-leader_portrait VARCHAR(255),
-leader_rank INT,
+leader_id INT AUTO_INCREMENT NOT NULL,
+leader_name VARCHAR(50) NOT NULL,
+leader_portrait VARCHAR(255) NOT NULL,
+leader_rank INT NOT NULL,
 PRIMARY KEY(leader_id)
 );
