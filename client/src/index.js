52b52525591ffca17bc341d71089285807683ac6
Andrew Jolivette <amjolivette@gmail.com>
2019-10-23 12:38:37 -0700

    added a new banner, background image, Cinzel font styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
+
+import './assets/fonts/Cinzel-Regular.ttf'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+  
--- client/src/pages/Start/index.js
+++ client/src/pages/Start/index.js
@@ -1,52 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Button, Container, Row, Col, Card } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 import styles from "./index.module.css"; 
-import Logo from "./pictures/ph.png";
-import DisplayStats from './../../Components/DisplayStats';
+import Logo from "./pictures/titlelogo.png";
 
-function Start(props) {
-  const [modalShow, setModalShow] = React.useState(false);
+class Start extends Component {
 
-  return (
-    <Container className="vh-100 d-flex justify-content-center">
-      <Row className={styles.cardRow}>
-        <Col xs={12}>
-          <Card className={styles.cardSize}>
-            <Card.Img src={Logo} fluid />
-            <Card.Body>
-              <Card.Subtitle className="mb-2 text-muted text-center">Vincit qui se vincit.</Card.Subtitle>
-              <Row className="mt-5">
-                <Col>
-                  <Row>Current Champion Component Goes Here</Row>
-                  <Row>
-                    <Button
-                      variant="primary"
-                      onClick={() => setModalShow(true)}
-                    >
-                      View Stats
+  componentDidMount() {
+    console.log("call leaderboard")
+  }
+
+  render() {
+    return (
+      <Container className="vh-100 d-flex justify-content-center">
+        <Row className={styles.cardRow}>
+          <Col xs={12}>
+            <Card className={`${styles.cardSize} border-0`}>
+              <Card.Img src={Logo} fluid />
+              <Card.Body className="bg-dark">
+                <Card.Subtitle className="mb-2 text-light text-center slogan">Vincit qui se vincit</Card.Subtitle>
+                <Row className="mt-2">
+                  <Col>
+                    <Row>Current Champion Component Goes Here</Row>
+                  </Col>
+                  <Col className="text-center">
+                    <Button 
+                      size="lg"
+                      onClick={() => this.props.handleState("create")}>
+                      Create Challenger
                     </Button>
-                  </Row>
-                </Col>
-                <Col className="text-center">
-                  <Button 
-                    size="lg"
-                    onClick={() => props.handleState("create")}>
-                    Create Challenger
-                  </Button>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <DisplayStats
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-    </Container>
-  )
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default Start;
