ce2a0c73737555f2415a5da256183ed57cb49805
Peter Wu <23109224+PeterBWu@users.noreply.github.com>
2019-10-25 09:42:29 -0700

    please work server

--- config/connections.js
+++ config/connections.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL||{
     host:"localhost",
     port:3306,
     user:"root",
