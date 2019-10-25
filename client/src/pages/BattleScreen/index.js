24419dda1ef1ca90a6f69ce734f300fb8b16be80
Paul Thomas <traitorpt@gmail.com>
2019-10-24 20:13:57 -0700

    fixed the battlescreen

--- client/src/pages/BattleScreen/index.js
+++ client/src/pages/BattleScreen/index.js
@@ -15,8 +15,8 @@ import { runInThisContext } from 'vm'
 
 const style = {
   image: {
-    backgroundPosition: 'center',
-    backgourndSize: 'auto-fill'
+    height: '35vh',
+    width: '15vw',
   }
 }
 
@@ -28,8 +28,6 @@ class BattleScreen extends React.Component {
     textResult: "Result",
     oppentHealth: 'col-12',
     playerHealth: 'col-12',
-    oppHealthCounter: 1,
-    playerHealthCounter: 1,
     userHp: this.props.currentState.characterStat.hp,
     userMaxHp: this.props.currentState.characterStat.hp,
     currentOpponent: [...this.props.currentState.challengers].reverse()[this.props.currentState.round - 1],
@@ -40,9 +38,9 @@ class BattleScreen extends React.Component {
   }
 
   checkUserLose = (userHp) => {
-      if (userHp <= 0){
-        this.props.handleState('end', true)
-      }
+    if (userHp <= 0) {
+      this.props.handleState('end', true)
+    }
   }
 
   checkOpponentLose = (opponentHp) => {
@@ -57,13 +55,13 @@ class BattleScreen extends React.Component {
       this.props.handleState('end', true)
     }
     let randomNumber = Math.floor(Math.random() * 3) + 1
-    switch (2) {
+    switch (randomNumber) {
       case 1:
         if (parseInt(this.state.userHp) <= 0) {
           this.props.handleState('end', true)
         }
         this.setState({
-          playerHealthCounter: parseInt(this.state.playerHealthCounter) + 1,
+
           userHp: parseInt(this.state.userHp) - Math.floor(parseInt(this.state.currentOpponent.leader_atk) / 2),
           textResult: 'You Took Damage',
           visualResult: Hit,
@@ -75,7 +73,6 @@ class BattleScreen extends React.Component {
         }
         this.setState({
           opponentHp: parseInt(this.state.opponentHp - Math.floor(parseInt(this.props.currentState.characterStat.attack) / 2)),
-          oppHealthCounter: parseInt(this.state.oppHealthCounter) + 1,
           textResult: 'You Cause Damage',
           visualResult: Damage,
         })
@@ -96,18 +93,23 @@ class BattleScreen extends React.Component {
     { this.checkOpponentLose(this.state.opponentHp) }
     return (
       <div className="container 100vh d-flex justify-content-center text-center mt-2">
+
+
         <div className="row bg-danger">
           <div className="col-lg-6 border border-dark">
             <div style={{ textAlign: 'center' }}>
-              <img  src={this.state.visualResult} 
-                    style={{  width: '30vw', 
-                              height: '30vh' }} />
+              <img src={this.state.visualResult}
+                style={{
+                  width: '30vw',
+                  height: '30vh'
+                }} />
               <h3 style={{ marginTop: '5vh', marginBottom: '4vh' }}> {this.state.textResult}</h3>
               <div className="row">
                 <div className="col-lg-6">
-                  <ImageCard
+                  <Card.Img
                     src={this.state.currentOpponent.leader_portrait}
                     size='12'
+                    style={style.image}
                   />
                 </div>
                 <div className="col-lg-6">
@@ -149,16 +151,18 @@ class BattleScreen extends React.Component {
 
 
                 <div classname="row">
-                    <div className="col-md-12">
-                      <div
-                        style={{ width: `${(this.state.userHp / this.state.userMaxHp) * 100}%`, 
-                        height: '9vh', 
-                        backgroundColor: 'green', 
-                        border: 'dotted blue 5px' }}>
-                      </div>
+                  <div className="col-md-12">
+                    <div
+                      style={{
+                        width: `${(this.state.userHp / this.state.userMaxHp) * 100}%`,
+                        height: '9vh',
+                        backgroundColor: 'green',
+                        border: 'dotted blue 5px'
+                      }}>
                     </div>
                   </div>
                 </div>
+
 
 
               </div>
@@ -171,23 +175,29 @@ class BattleScreen extends React.Component {
                 </div>
               </div>
             </div>
-            <div style={{ border: 'black solid 1.5px', 
-                          borderRadius: '6  px',
-                          marginTop: '2vh' }}>
+
+            <div style={{
+              border: 'black solid 1.5px',
+              borderRadius: '6px',
+              marginTop: '10vh'
+            }}>
               <Card>
                 <Container >
                   <Row>
                     <FightButton id={'rock'}
                       onClick={this.handleClick}
                       type="Grapple"
+                      size="4"
                     />
                     <FightButton id={'paper'}
                       onClick={this.handleClick}
                       type="Defend"
+                      size="4"
                     />
                     <FightButton id={'scissors'}
                       onClick={this.handleClick}
-                      type="Grapple"
+                      type="Attack"
+                      size="4"
                     />
                   </Row>
                 </Container>
@@ -195,8 +205,9 @@ class BattleScreen extends React.Component {
             </div>
           </div>
         </div>
-     
-    
+      </div>
+
+
     )
   }
 }
